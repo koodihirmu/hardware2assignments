b7fdc8221187c9aa1623b1645d7621ffefc91e01
Tomi <59865056+koodihirmu@users.noreply.github.com>
2025-03-17 14:32:58 +0200

    assigment 1.1 ready

--- test.py
+++ test.py
@@ -2,17 +2,47 @@ import time
 from machine import UART, Pin, I2C, Timer, ADC
 from ssd1306 import SSD1306_I2C
 
-button = Pin(9, Pin.IN, Pin.PULL_UP)
+# Button class for debouncing etc
+class Button:
+    def __init__(self, pin, debounce_ms) -> None:
+        self.pin = Pin(pin, Pin.IN, Pin.PULL_UP)
+        self.debounce_ms = debounce_ms
+    
+    def pressed(self) -> bool:
+        if (not self.pin()):
+            time.sleep_ms(self.debounce_ms)
+            if (not self.pin()):
+                return True
+        return False
+
+sw0 = Button(9, 100)
+sw1 = Button(8, 100)
+sw2 = Button(7, 100)
+
 i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
 oled_width = 128
 oled_height = 64
 oled = SSD1306_I2C(oled_width, oled_height, i2c)
-oled.fill(0)
-oled.text('Hello World', 0, 0, 1)
-oled.show()
-oled.fill(0)
-oled.text('Button pressed', 0, 10, 1)
 
-while button() == 1:
-    time.sleep(0.05)
+pos_x = int(oled_width/2 - 15)
+pos_y = oled_height - 8
+oled.text("<=>", pos_x, pos_y, 1)
+speed = 5
+
+while True:
+
+    # move to the right
+    if (sw2.pressed()):
+        oled.fill(0)
+        if pos_x < 128 - 30:
+            pos_x += speed
+        oled.text("<=>", pos_x, pos_y, 1)
+
+    # move to the left
+    if (sw0.pressed()):
+        oled.fill(0)
+        if pos_x > 0:
+            pos_x -= speed
+        oled.text("<=>", pos_x, pos_y, 1)
+
     oled.show()
