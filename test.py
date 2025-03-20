8f050211768e894438bb6c97fd9d2545a7f0cbf2
Tomi <59865056+koodihirmu@users.noreply.github.com>
2025-03-20 23:30:07 +0200

    fixes

--- test.py
+++ test.py
@@ -35,21 +35,20 @@ for samples in range(1, 10 * 250):
     value = data.get()
 
     max_scale = 32
-    time_scale = 0.2
-    time_scale = 0.1
+    time_scale = 0.50
 
     # scale the value between 0 and 100 or whatever we want
     scaled_value = (value - min)/(max - min) * max_scale
 
-    # show the pixels on the screen
-    oled.pixel(int(samples * time_scale) % 128, pos_y +
-    oled.pixel(int(samples * time_scale) % (oled_width), pos_y +
-               int(scaled_value - max_scale/2), 1)
+    # don't draw the first (0) pixel because it shows up weird
+    if (int(samples*time_scale) % oled_width > 0):
+        # draw the pixels on the screen
+        oled.pixel(int(samples * time_scale) % (oled_width), pos_y +
+                   int(scaled_value - max_scale/2), 1)
 
-    if not (samples % 50):
+    # show the pixels every time the screen if filled with the data
     if ((samples * time_scale) % oled_width == 0) and samples != 0:
         oled.show()
-
         oled.fill(0)
 
     print(scaled_value)
